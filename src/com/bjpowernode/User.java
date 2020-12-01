f11c58ce187c9bcf23a4e0f920d698df8e8463fb
Administrator <2170643294@qq.com>
2020-12-01 17:05:00 +0800

    模块初始提交

---
+++ src/com/bjpowernode/User.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+public class User {
+    public static void main(String[] args) {
+        int i = 1;
+
+        int j = 2;
+
+        System.out.println(i + j);
+    }
+}
